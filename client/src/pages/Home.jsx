e72c3a7084a6e163f523e10f20dc34cf4835ab66
Rajiv S <rajiv620019@gmail.com>
2023-07-21 19:07:45 +0530

    Implemented the states for store using valtio

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,3 +1,14 @@
+import { motion, AnimatePresence } from "framer-motion";
+import { useSnapshot } from "valtio";
+import state from "../store";
+
+import {
+  headContainerAnimation,
+  headContentAnimation,
+  headTextAnimation,
+  slideAnimation,
+} from "../config/motion";
+
 const Home = () => {
   return <h1>Home</h1>;
 };
