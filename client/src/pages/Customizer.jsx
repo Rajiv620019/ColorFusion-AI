2001cbbaeadf2aef1d342f95b5ff851e568f5b39
Rajiv S <rajiv620019@gmail.com>
2023-07-25 00:22:06 +0530

    Created files for shirt 3d model

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -17,6 +17,53 @@ import {
 } from "../components";
 
 const Customizer = () => {
-  return <div>Custom</div>;
+  const snap = useSnapshot(state);
+
+  return (
+    <AnimatePresence>
+      {!snap.intro && (
+        <>
+          <motion.div
+            key="custom"
+            className="absolute top-0 left-0 z-10"
+            {...slideAnimation("left")}
+          >
+            <div className="flex items-center min-h-screen">
+              <div className="editortabs-container tabs">
+                {EditorTabs.map((tab) => (
+                  <Tab key={tab.name} tab={tab} handleClick={() => {}} />
+                ))}
+              </div>
+            </div>
+          </motion.div>
+
+          <motion.div
+            className="absolute z-10 top-5 right-5"
+            {...fadeAnimation}
+          >
+            <CustomButton
+              type="filled"
+              title="Go Back"
+              handleClick={() => (state.intro = true)}
+            />
+          </motion.div>
+          <motion.div
+            className="filtertabs-container"
+            {...slideAnimation("up")}
+          >
+            {EditorTabs.map((tab) => (
+              <Tab
+                key={tab.name}
+                tab={tab}
+                isFilterTab
+                isActiveTab=""
+                handleClick={() => {}}
+              />
+            ))}
+          </motion.div>
+        </>
+      )}
+    </AnimatePresence>
+  );
 };
 export default Customizer;
