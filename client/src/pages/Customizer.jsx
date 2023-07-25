9b9b9b5124397b5babbfb506effb32f9c0be9af3
Rajiv S <rajiv620019@gmail.com>
2023-07-25 23:07:43 +0530

    Implemented Active logo and active texture

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -36,12 +36,43 @@ const Customizer = () => {
       case "colorPicker":
         return <ColorPicker />;
       case "filePicker":
-        return <FilePicker />;
+        return <FilePicker file={file} setFile={setFile} />;
       case "aipicker":
         return <AIPicker />;
       default:
         return null;
     }
+  };
+
+  const handleDecals = (type, result) => {
+    const decalType = DecalTypes[type];
+
+    state[decalType.stateProperty] = result;
+
+    if(!activeFilterTab[decalType.filterTab]) {
+      handleActiveFilterTab(decalType.filterTab)
+    }
+  }
+
+  const handleActiveFilterTab = (tabName) => {
+    switch (tabName) {
+      case "logoShirt":
+          state.isLogoTexture = !activeFilterTab[tabName];
+        break;
+      case "stylishShirt":
+          state.isFullTexture = !activeFilterTab[tabName];
+        break;
+      default:
+        state.isLogoTexture = true;
+        state.isFullTexture = false;
+        break;
+    }
+
+  const readFile = (type) => {
+    reader(file).then((result) => {
+      handleDecals(type, result);
+      setActiveEditorTab("");
+    });
   };
 
   return (
