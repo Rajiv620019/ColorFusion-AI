8e39246da078be2e2e80edb44c93c2c90a741ba5
Rajiv S <rajiv620019@gmail.com>
2023-07-25 17:58:37 +0530

    Setting up shirt ambient and shadow

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -1,13 +1,19 @@
-import { Center, Environment } from "@react-three/drei";
+import { Backdrop, Center, Environment } from "@react-three/drei";
 import Shirt from "./Shirt";
 import CameraRig from "./CameraRig";
 
 const Canvas = () => {
   return (
-    <Canvas>
+    <Canvas
+      shadows
+      camera={{ position: [0, 0, 0], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+      className="w-full max-w-full transition-all ease-in"
+    >
       <ambientLight intensity={0.5} />
       <Environment preset="city" />
       <CameraRig>
+        <Backdrop />
         <Center>
           <Shirt />
         </Center>
